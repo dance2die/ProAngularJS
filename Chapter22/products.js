9a3624eb4fad636b93783b9a69bac73edcd1993a
dance2die <dontbothermewithspam@gmail.com>
2015-02-25 23:25:49 -0500

    Still not working after disabling html5Mode for location provider

--- Chapter22/products.js
+++ Chapter22/products.js
@@ -1,77 +1,90 @@
 ﻿angular.module("exampleApp", ["increment", "ngResource", "ngRoute"])
 .constant("baseUrl", "http://localhost:5500/products/")
+.factory("productsResource", function ($resource, baseUrl) {
+    return $resource(baseUrl + ":id", { id: "@id" },
+            { create: { method: "POST" }, save: { method: "PUT" } });
+})
 .config(function ($routeProvider, $locationProvider) {
 
-	$locationProvider.html5Mode(true);
+    //$locationProvider.html5Mode(true);
 
-	$routeProvider.when("/list", {
-		templateUrl: "/tableView.html"
-	});
+    $routeProvider.when("/edit/:id", {
+        templateUrl: "/editorView.html",
+        controller: "editCtrl"
+    });
 
-	$routeProvider.when("/edit", {
-		templateUrl: "/editorView.html"
-	});
+    $routeProvider.when("/create", {
+        templateUrl: "/editorView.html",
+        controller: "editCtrl"
+    });
 
-	$routeProvider.when("/create", {
-		templateUrl: "/editorView.html"
-	});
+    $routeProvider.otherwise({
+        templateUrl: "/tableView.html",
+        controller: "tableCtrl",
+        resolve: {
+            data: function (productsResource) {
+                return productsResource.query();
+            }
+        }
+    });
+})
 
-	$routeProvider.otherwise({
-		templateUrl: "/tableView.html"
-	});
+.controller("defaultCtrl", function ($scope, $location, productsResource) {
+
+    $scope.data = {};
+
+    $scope.createProduct = function (product) {
+        new productsResource(product).$create().then(function (newProduct) {
+            $scope.data.products.push(newProduct);
+            $location.path("/list");
+        });
+    }
+
+    $scope.deleteProduct = function (product) {
+        product.$delete().then(function () {
+            $scope.data.products.splice($scope.data.products.indexOf(product), 1);
+        });
+
+        $location.path("/list");
+    }
+})
+.controller("tableCtrl", function ($scope, $location, $route, data) {
+    $scope.data.products = data;
+
+    $scope.refreshProducts = function () {
+        $route.reload();
+    }
 
 })
-.controller("defaultCtrl", function ($scope, $http, $resource, baseUrl) {
+.controller("editCtrl", function ($scope, $routeParams, $location) {
 
-	$scope.displayMode = "list";
-	$scope.currentProduct = null;
+    $scope.currentProduct = null;
 
-	$scope.productsResource = $resource(baseUrl + ":id", { id: "@id" },
-            { create: { method: "POST" }, save: { method: "PUT" } });
+    if ($location.path().indexOf("/edit/") == 0) {
+        var id = $routeParams["id"];
+        for (var i = 0; i < $scope.data.products.length; i++) {
+            if ($scope.data.products[i].id == id) {
+                $scope.currentProduct = $scope.data.products[i];
+                break;
+            }
+        }
+    }
 
-	$scope.listProducts = function () {
-		$scope.products = $scope.productsResource.query();
-	}
+    $scope.cancelEdit = function () {
+        $location.path("/list");
+    }
 
-	$scope.deleteProduct = function (product) {
-		product.$delete().then(function () {
-			$scope.products.splice($scope.products.indexOf(product), 1);
-		});
-		$scope.displayMode = "list";
-	}
+    $scope.updateProduct = function (product) {
+        product.$save();
+        $location.path("/list");
+    }
 
-	$scope.createProduct = function (product) {
-		new $scope.productsResource(product).$create().then(function (newProduct) {
-			$scope.products.push(newProduct);
-			$scope.displayMode = "list";
-		});
-	}
-
-	$scope.updateProduct = function (product) {
-		product.$save();
-		$scope.displayMode = "list";
-	}
-
-	$scope.editOrCreateProduct = function (product) {
-		$scope.currentProduct = product ? product : {};
-		$scope.displayMode = "edit";
-	}
-
-	$scope.saveEdit = function (product) {
-		if (angular.isDefined(product.id)) {
-			$scope.updateProduct(product);
-		} else {
-			$scope.createProduct(product);
-		}
-	}
-
-	$scope.cancelEdit = function () {
-		if ($scope.currentProduct && $scope.currentProduct.$get) {
-			$scope.currentProduct.$get();
-		}
-		$scope.currentProduct = {};
-		$scope.displayMode = "list";
-	}
-
-	$scope.listProducts();
+    $scope.saveEdit = function (product) {
+        if (angular.isDefined(product.id)) {
+            $scope.updateProduct(product);
+        } else {
+            $scope.createProduct(product);
+        }
+        $scope.currentProduct = {};
+    }
 });
